b392a02c7d14184244a283cff40ba19a71a83d0a
siraj <sirajbhutta50@gmail.com>
2022-10-02 19:55:00 +0500

    update(first app): counter app is added

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-//first js line
-document.write('Hello world')
+//Js Element
+let name = `JavaScript Projects`
+let para = `js apps are build with vanilla js and designed with design source, ui design system.`
+document.getElementById("app_name").innerHTML = name;
+document.getElementById("app_des").innerHTML = para;
+//greetings
+const greet = () => {
+    let greeting = "Hello World"
+    document.getElementById("Greet").innerHTML = greeting;
+}
+//counter app
+let app_name1 = `Counter App`
+document.getElementById("first_app_name").innerHTML = app_name1;
+let app_des1 = `simple counter app usefull in webapps and commerce applications`
+document.getElementById("first_app_des").innerHTML = app_des1;
 
-// stop watch
+let i = 0;
+let Inc = () => {
+        i = i + 1;
+    console.log(i)
+    document.getElementById("counter").innerHTML = i;
+}
+let Dec = () => {
+            i = i - 1;
+    console.log(i)
+    document.getElementById("counter").innerHTML = i;
+}              
