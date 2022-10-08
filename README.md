88010ef0768a1c2598d439fdc7eae3f0b521f8f5
siraj <sirajbhutta50@gmail.com>
2022-10-08 09:51:47 +0500

    Update (New App): BMI Calculater

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ simple counter app usefull in webapps and commerce applications.
 #### Review page app
 
 Simple Coustomers review section on any website.
+
+#### BMI Calculator
+
+Bmi Calculater using simple logic.
--- main.js
+++ main.js
@@ -13,8 +13,6 @@ let app_name1 = `Counter App`
 document.getElementById("first_app_name").innerHTML = app_name1;
 let app_des1 = `simple counter app usefull in webapps and commerce applications`
 document.getElementById("first_app_des").innerHTML = app_des1;
-
-
 let i = 0;
 let Inc = () => {
     i = i + 1;
@@ -26,19 +24,16 @@ let Dec = () => {
     console.log(i)
     document.getElementById("counter").innerHTML = i;
 }
-
 //review app
 let app_name2 = `Review page app`
 document.getElementById("sec_app_name").innerHTML = app_name2;
 let app_des2 = `Simple Coustomers review section on any website.`
 document.getElementById("sec_app_des").innerHTML = app_des2;
-
-
 let btn = document.getElementById('review_button');
 let says = document.getElementById("review");
 let person = document.getElementById('person');
 let post = document.getElementById('position')
-
+//reviews Data
 const reviews = [{
         says: `"The best way to find yourself is to lose yourself in the service of others."`,
         person: ` indra Gandhi`,
@@ -60,7 +55,7 @@ const reviews = [{
         post: `co`,
     },
 ];
-
+//reviews logic
 const Review = () => {
     const random = Math.floor(Math.random() * reviews.length);
     says.innerText = reviews[random].says;
@@ -68,18 +63,26 @@ const Review = () => {
     post.innerText = reviews[random].post;
 }
 btn.addEventListener('click', Review);
-
-
-
 //responsive nav
-let app_name3 = `Responsive Nav bar`
+let app_name3 = `Responsive Navbar`
 document.getElementById("third_app_name").innerHTML = app_name3;
 let app_des3 = `Simple responsive navbar app.`
-document.getElementById("third_app_des").innerHTML = app_des3;
+document.getElementById("third_app_des").innerHTML = app_des3
+// const hamburger = document.querySelector('.hamburger');
+// const navLink = document.querySelector('.nav__link');
+// hamburger.addEventListener('click', () => {
+//   navLink.classList.toggle('hide');
+// });
+//BMI calculater
+let app_name4 = `BMI Calculater`;
+document.getElementById("four_app_name").innerHTML = app_name4;
+let app_des4 = `Bmi Calculater using simple logic`;
+document.getElementById("four_app_des").innerHTML = app_des4;
+const Bmi = () => {
+    let w = document.getElementById('wieght').value;
+    let h = document.getElementById('hieght').value;
+    const b = w / (h * h);
+    document.getElementById("bmi").innerHTML = b;
+}
 
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
 
-hamburger.addEventListener('click', () => {
-  navLink.classList.toggle('hide');
-});
